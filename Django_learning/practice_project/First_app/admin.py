f876cfbe7eb503e83fdb0e0eb5fc2cec101187a5
Vivek Soni <vivek1.citrusbug@gmail.com>
2024-11-08 12:39:38 +0530

    Student table created

--- Django_learning/practice_project/First_app/admin.py
+++ Django_learning/practice_project/First_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import students
 
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("student_name","student_id","join_date","result")
+
+admin.site.register(students,MemberAdmin)
 # Register your models here.
